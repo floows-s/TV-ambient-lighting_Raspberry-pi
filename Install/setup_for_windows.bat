ea99a30a9886714c7ed9d699374ef922adfe27ac
unknown <flvrij03@gmail.com>
2025-09-21 21:03:13 +0200

    Downloads and extracts OpenCV to C folder

--- Install/setup_for_windows.bat
+++ Install/setup_for_windows.bat
@@ -1,1 +1,7 @@
-echo "Work in progress!";
+echo "Work in progress!"
+
+mkdir temp
+curl -L -o .\temp\opencv-4.12.0-windows.exe https://github.com/opencv/opencv/releases/download/4.12.0/opencv-4.12.0-windows.exe
+
+start /wait .\temp\opencv-4.12.0-windows.exe -o"C:\" -y
+rmdir /s /q .\temp
