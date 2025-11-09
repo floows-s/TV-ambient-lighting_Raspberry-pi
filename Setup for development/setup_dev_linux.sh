c66296252ab0981c6538c9e6e0bdf2aabbf74aec
unknown <flvrij03@gmail.com>
2025-11-09 17:51:59 +0100

    Added setup_dev_linux.sh

---
+++ Setup for development/setup_dev_linux.sh
@@ -0,0 +1,50 @@
+echo "This is the setup script for the ambi light (raspberry pi) project made by floows.";
+sudo apt update && sudo apt upgrade
+
+# cmake
+echo "Installing cmake..."
+sudo apt install -y cmake
+
+# --- Librarys ---
+mkdir ../Librarys/ && cd ../Librarys
+
+# rpi_ws2811x
+echo "Installing and building rpi_ws281x..."
+git clone https://github.com/jgarff/rpi_ws281x.git
+cd rpi_ws281x
+mkdir build && cd build
+cmake -D BUILD_SHARED=OFF ..
+cmake --build .
+cd ../../
+
+# opencv
+echo "About to install and build OpenCV..."
+echo "The OpenCV lib takes a long time to build on a raspberry pi! It took me more than a few hours."
+while true; do
+    read -p "Do you wish to continue? (y/n) " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Note: needed for opencv to build
+sudo apt install -y libgtk2.0-dev pkg-config 
+sudo apt install -y g++ wget unzip
+cd ../Librarys
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
+unzip opencv.zip
+cd opencv-4.x
+mkdir -p build && cd build
+cmake -D BUILD_SHARED=OFF ..
+cmake --build
+cd ../../
+
+echo "Everything is installed!" 
+echo "Building project..."
+mkdir Build && cd Build
+cmake -G "Unix Makefile" ..
+make
+
+echo "Finished!"
